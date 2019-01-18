b979c556f37ba58d2abbd84e12d3ab5bdb5e2be4
H B <phajib@gmail.com>
2019-01-18 18:57:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.map {|name| [badge_maker(name)]}
+  attendees.map {|name| badge_maker(name)}
 end
 
 def assign_rooms(attendees)
