2b4f25276116680cc85fbb54f87752e79deb2914
H B <phajib@gmail.com>
2019-01-18 18:59:25 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require 'benchmark'
+puts Benchmark.measure { 10_000_000.times { Object.new } }
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map {|name| badge_maker(name)}
+end
+
+def assign_rooms(attendees)
+  x = 0
+  attendees.map do |name|
+    x += 1
+    "Hello, #{name}! You'll be assigned to room #{x}!"
+  end
+end
+
+def printer(attendees)
+  x = 0
+  attendees.each {|name| puts "Hello, my name is #{name}."}
+  attendees.each do
+    |name|
+    x += 1
+    puts "Hello, #{name}! You'll be assigned to room #{x}!"
+  end
+end
